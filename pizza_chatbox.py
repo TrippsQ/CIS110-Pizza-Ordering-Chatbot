cfca553632a61de1507b3cd5d699a287d5b09b1e
Quasean Nicholson <Quaseanz@gmail.com>
2025-08-09 23:23:46 -0400

    Fix pizza chatbot input handling and price calculation

--- pizza_chatbox.py
+++ pizza_chatbox.py
@@ -1,7 +1,10 @@
 print("Welcome to the Pizza Ordering Chatbot! What would you like to order?")
 print("I am going to ask you a few questions. After typing an answer press enter")
-userName = input("\nEnter your name:")
-print(f"\nHello, {userName}. Nice to meet you!")
+userName = input("\nEnter your name:  ")
+if userName.lower() == "lord isszac":
+    print(f"\nMy creator, {userName}. Pleasure to serve you!")
+else: 
+    print(f"\nHello, {userName}. Nice to meet you.")
 
 size = input("What size do you want? Enter small, nediun, or large: ")
 FLavor = input("\nEnter the Flavor of the Pizza:   ")
@@ -9,11 +12,31 @@ crustType = input("\nwhat type of crust do you want:  ")
 quantity = input("\nHow many of these do vou want to order? Please use a interger.")
 quantity = int(quantity)
 method = input("\nIs this carry out or delivery")
+if method.lower() == "delivery":
+    deliveryFee = 5
+else:
+    deliveryFee = 0
 
 salesTax= 1.1
-pizzaCost = 14.99
-total = (pizzaCost * quantity) * salesTax
+
+if size.lower() == "small":
+    pizzaCost = 8.99 
+elif size.lower() == "medium":
+    pizzaCost = 14.99 
+elif size.lower() == "large":
+    pizzaCost = 17.99
+
+total = (pizzaCost * quantity) * salesTax + deliveryFee
 
 print("-" * 10)
 print(f"Thank you, {userName}, for your order")
 print(f"Your {quantity} {size} {FLavor} pizzas with {crustType} costs ${total: ,.2f}.")
+if total >= 50: 
+    print("\nCongrats, You earned 10$ off your next order")
+else:
+    print("Orders over 50$ receive 10$ off.")
+
+
+print("-" * 10)
+
+
